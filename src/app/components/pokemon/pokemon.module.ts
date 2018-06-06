5f43c0628a5003ae5843d1b01ce88370ec0ef012
Douglas <douglasmonteiro@icloud.com>
2018-06-06 00:45:48 -0300

    adicionando Pipe e Tabela

--- src/app/components/pokemon/pokemon.module.ts
+++ src/app/components/pokemon/pokemon.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonComponent } from './pokemon.component';
+import { FiltraPorNomePipe } from './filtraPorNome.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PokemonComponent],
-  exports: [PokemonComponent]
+  declarations: [
+    PokemonComponent,
+    FiltraPorNomePipe
+  ],
+  exports: [
+    PokemonComponent,
+    FiltraPorNomePipe
+  ]
 })
 export class PokemonModule { }
