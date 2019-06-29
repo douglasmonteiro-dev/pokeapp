87e89c52889cb7c11ce8c8a0ebabef9ffc3390ee
Douglas Monteiro <douglasmonteiro@icloud.com>
2019-06-28 23:10:05 -0300

    Revert "Auto-generated commit"
    This reverts commit 17e8908cb69268a042e2c402344a09381eabd612.

---
+++ src/app/routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { Page404Component } from './pages/page404/page404.component';
+import { ListaComponent } from './pages/lista/lista.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
+
+const rotasDaApp: Routes = [
+    {path: '', component: ListaComponent},
+    {path: 'detalhes/:name/:url', component: DetalhesComponent},
+    {path: '**', component: Page404Component }
+];
+
+export const roteamento = RouterModule.forRoot(rotasDaApp);
